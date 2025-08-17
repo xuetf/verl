6b95f07f09cd2cbbb6c8caaa0cc696d3a845108c
xuetaofeng <xuetaofeng@meituan.com>
2025-08-17 13:49:27 +0800

    init

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ install_requires = [
     "pylatexenc",
     "ray[default]>=2.41.0",
     "torchdata",
-    "tensordict>=0.8.0,<=0.9.1,!=0.9.0",
-    "transformers",
+    "tensordict==0.7.2",
+    "transformers==4.51.3",
     "wandb",
     "packaging>=20.0",
     "tensorboard",
@@ -49,11 +49,11 @@ PRIME_REQUIRES = ["pyext"]
 GEO_REQUIRES = ["mathruler", "torchvision", "qwen_vl_utils"]
 GPU_REQUIRES = ["liger-kernel", "flash-attn"]
 MATH_REQUIRES = ["math-verify"]  # Add math-verify as an optional dependency
-VLLM_REQUIRES = ["tensordict>=0.8.0,<=0.9.1,!=0.9.0", "vllm>=0.7.3,<=0.9.1"]
+VLLM_REQUIRES = ["tensordict==0.7.2", "vllm>=0.7.3,<=0.9.1"]
 SGLANG_REQUIRES = [
-    "tensordict>=0.8.0,<=0.9.1,!=0.9.0",
+    "tensordict==0.7.2",
     "sglang[srt,openai]==0.4.9.post6",
-    "torch==2.7.1",
+    "torch==2.6.0",
 ]
 TRL_REQUIRES = ["trl<=0.9.6"]
 MCORE_REQUIRES = ["mbridge"]
